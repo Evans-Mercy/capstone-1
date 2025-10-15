ea12ff500d8d72ad3513137c8412df204a5f923c
Evans-Mercy <cheria.mercy@gmail.com>
2025-10-15 10:57:59 -0500

    Added transactions class

---
+++ src/main/java/com/pluralsight/Transactions.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class Transactions {
+
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    //Constructor
+    public Transactions(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    //Getters
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    //To string method to print
+    //ask Dave to remind me the override explanation
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
